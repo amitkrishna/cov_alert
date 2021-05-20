9fb3899a59b67059b6997f5195bf630f3804a804
amitkrishna <amitkrishna936@gmail.com>
2021-05-20 10:25:45 +0530

    bug fix

--- main.py
+++ main.py
@@ -10,6 +10,18 @@ myDOMAIN = str(input("Enter mailgun domain name"))
 myMAIL = str(input("Enter mailgun sandbox id"))
 myAPI = "MAILGUN_API_KEY"
 minAGE = str(input("Enter minimum age for Vaccination"))
+msgStr = ""
+
+
+def send_simple_message(MY_EMAIL, MY_DOMAIN, MY_MAIL, message_string):
+	return requests.post(
+            f"https://api.mailgun.net/v3/{MY_DOMAIN}/messages",
+          		auth=("api", MY_API),
+          		data={"from": f"{MY_MAIL}",
+                            "to": [MY_EMAIL],
+                            "subject": "Alert on Covid 19 Vaccination",
+                            "text": message_string})
+
 while True:
     toDay = time.strftime("%d/%m/%Y")
     _url = f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={pinCODE}&date={toDay}"
@@ -24,17 +36,7 @@ while True:
         for center in response['centers']:
             for session in center['sessions']:
                 if (session['min_age_limit'] != minAGE) & (session['available_capacity'] > 0):
-                    msgStr=f"Hej: {toDay}'s Alert'!! \n\n Available Slots - {session['available_capacity']} in center {center['name']} on the session {session['date']} for the minimum age {session['min_age_limit']}"
-                    send_simple_message(myEMAIL, myDOMAIN, myMAIL, msgStr)
-        time.sleep(1000)
-
-
-def send_simple_message(MY_EMAIL, MY_DOMAIN, MY_MAIL, message_string):
-	return requests.post(
-            f"https://api.mailgun.net/v3/{MY_DOMAIN}/messages",
-          		auth=("api", MY_API),
-          		data={"from": f"{MY_MAIL}",
-                            "to": [MY_EMAIL],
-                            "subject": "Alert on Covid 19 Vaccination",
-                            "text": message_string})
-
+                    msgStr+=f"Hej: {toDay}'s Alert'!! \n\n Available Slots - {session['available_capacity']} in center {center['name']} on the session {session['date']} for the minimum age {session['min_age_limit']}"
+        
+    send_simple_message(myEMAIL, myDOMAIN, myMAIL, msgStr)
+time.sleep(1000)
